8f7e1a6c0d6e7223a3e92fe223056cc65b201162
Jussi Judin <jussi@jussiblet.vpn>
2009-05-26 20:49:11 +0300

    Adding copyright notices and license.

--- winever.sh
+++ winever.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2009 Jussi Judin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 WINE_PREFIX="$1"
 VERSION="$2"
@@ -54,6 +68,8 @@ else
     for PROG in *; do
         alias "$PROG"="$PROGRAMPATH"/"$PROG"
     done
+    # Needed for winetricks to work.
+    export WINE="$PROGRAMPATH"/wine
     if test -n "`env | grep MANPATH_PREWINE`"; then
         export MANPATH="$MANPATH_PREWINE"
     fi
